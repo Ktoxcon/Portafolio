d65caea68e9632b7e16ed5c7f0ee5475ab256d91
Ktoxcon <kennethtoxro@gmail.com>
2020-07-05 20:01:55 -0600

    inde.jsx of projects page fixed

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../../components/navbar/navbar.component";
+import { NavBar } from "../../components/navbar/navbar.component";
 
 const Index = () => {
   return (
