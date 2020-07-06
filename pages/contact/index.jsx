1dbb0dcc389f33cddb1d58a9a5f35c1cbb5f6f3b
Ktoxcon <kennethtoxro@gmail.com>
2020-07-05 23:07:41 -0600

    Minor fixes in the Navbar Brand, new title added to the contact section

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Head from "next/head";
 import Banner from "../../components/banner/banner.component";
 import NavBar from "../../components/navbar/navbar.component";
 
 const Index = () => {
   return (
     <>
+      <Head>
+        <title>Contact</title>
+      </Head>
       <NavBar />
       <div>
         <Banner title="Conoceme" />
